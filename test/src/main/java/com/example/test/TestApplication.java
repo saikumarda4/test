45a0a8dda319d8be6b2832f98b7b8df3b4e2eb6a
saikumar <saikumarda4@gmail.com>
2017-12-13 15:45:31 +0530

    done

---
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController	
+
+
+public class TestApplication {
+	 @RequestMapping("/hai")
+	   
+	    String home() 
+	 	{
+	        return "Hello World!";
+	    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
---
+++ test/src/main/java/com/example/test/repo/Book.java
@@ -0,0 +1,27 @@
+package com.example.test.repo;
+
+public class Book {
+	private int id;
+	private String author;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
